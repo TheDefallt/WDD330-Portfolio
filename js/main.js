e6c42d3df0305690489c356639d7ff47095a37b4
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-24 14:36:31 -0700

    Style Updates

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
-//function buildTOC(){
-    const links = [
-        {
-            label: "Week 1",
-            url: "week1/index.html"
-        },
-        {
-            label: "Week 2",
-            url: "week2/index.html"
-        },
-        {
-            label: "Week 3",
-            url: "week3/index.html"
-        },
-        {
-            label: "Week 4",
-            url: "week4/index.html"
-        },
-        {
-            label: "Week 5",
-            url: "Week5_6 Midterm/index.html"
-        }
-    ];
+const links = [
+    {
+        label: "Week 1",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week 2",
+        url: "week2/index.html"
+    },
+    {
+        label: "Week 3",
+        url: "week3/index.html"
+    },
+    {
+        label: "Week 4",
+        url: "week4/index.html"
+    },
+    {
+        label: "Week 5",
+        url: "Week5_6%20Midterm/index.html"
+    }
+];
 
-    var ol = document.getElementById('tableofcontents');
+let ul = document.getElementById('tableofcontents');
 
-    links.forEach(element => {
-        
-        var a = document.createElement('a');
-        a.append(element.label);
-        a.href = element.url;
+links.forEach(element => {
+    let li = document.createElement('li');
+    li.setAttribute('class', 'weekItem');
+    li.innerHTML = `<a href=${element.url}>${element.label}</a>`;
 
-        var li = document.createElement('li');
-        li.appendChild(a);
-
-        ol.appendChild(li);
-    });
-//}
-
-//document.addEventListener("DOMContentLoaded", buildTOC);
+    ul.appendChild(li);
+});
