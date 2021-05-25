8cabe3b6531dbc24ccd46cee5d28e6ebfebaf9d0
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-25 15:28:26 -0700

    Midterm work

--- Week5_6 Midterm/midterm/main.js
+++ Week5_6 Midterm/midterm/main.js
@@ -1,68 +1,32 @@
-//Array for storing ToDo items
-//const toDoListData = [];
 import ToDo from './midterm.js'; 
 
 const toDo = new ToDo('toDoList');
 
 //Grab adding form
 const addForm = document.forms['addToDo'];
-const addToDoContent = addForm.elements.content;
 
-const itemForms = document.forms['toDoItem'];
-console.log(itemForms);
+//Grab add form input text field
+const addToDoContent = addForm.elements.content;
 
 addForm.addEventListener ('submit', (event) => {
     event.preventDefault();
     toDo.addToDo(addToDoContent.value);
+    addItemListeners();
     addForm.reset();
 });
 
+function addItemListeners() {
+    let itemInputs = document.forms['toDoForm'].elements;
 
+    itemInputs.forEach(itemInput => {
+        itemInput.addEventListener ('change', (event) => {
+            event.preventDefault();
+            toDo.toggleCompleteItem(itemInput.value)
+        });
+    });
 
+}
 
-
-
-//Object with constructor for ToDo items
-// const ToDo = function(content){
-//     this.id = Date.now(),
-//     this.content = content,
-//     this.completed = false
-// }
-
-// function addToDo(event){
-//     const todoItem = new ToDo(
-//         addForm.elements.content.value
-//         );
-//         toDoListData.push(todoItem);
-
-//         renderNewListItem(todoItem);
-    
-//     addForm.reset();
-//     event.preventDefault();
-// }
-
-// function renderNewListItem(toDoItemData){
-//     let toDoItem = document.createElement('li');
-
-//     let toDoCheck = document.createElement('input');
-//     toDoCheck.setAttribute('type', 'checkbox');
-
-//     let toDoContent = document.createElement('p');
-//     toDoContent.append(toDoItemData.content);
-
-//     let deleteForm = document.createElement('form');
-//     let deleteButton = document.createElement('button');
-//     deleteButton.setAttribute('type', 'submit');
-//     deleteForm.appendChild(deleteButton);
-
-//     toDoItem.appendChild(toDoCheck);
-//     toDoItem.appendChild(toDoContent);
-//     toDoItem.appendChild(deleteForm);
-
-//     console.log(toDoItem);
-
-//     toDoList.appendChild(toDoItem);
-// }
 
 //Array for tasks to be stored in + functions to write and read them from file
 
--- Week5_6 Midterm/midterm/midterm.js
+++ Week5_6 Midterm/midterm/midterm.js
@@ -18,6 +18,16 @@ export default class ToDo {
         this.renderToDoList();
     }
 
+    toggleCompleteItem(id) {
+        console.log("Toggled");
+
+        toDoItems.find(item => {
+            if (item.id === id){
+                console.log(todo.content);
+            }
+        });
+    }
+
     renderToDoList(){
         //Add strikethrough to completed items.
 
@@ -33,28 +43,16 @@ export default class ToDo {
     renderToDoItem(todo) {
         const item = document.createElement('li');
         item.setAttribute('class', 'toDoItem');
+        item.setAttribute('name', 'toDoItem');
 
         item.innerHTML = `
-            <input type="checkbox"/>
+            <input type="submit" value="${todo.id}" name="checkComplete"/>
             <p>${todo.content}</p>
-            <form>
-                <input type="hidden" value="${todo.id}">
-                <button type="submit">X</button>
-            </form>
+            <button type="submit" value="${todo.id}">X</button>
         `;
         return item;
     }
-
-    toggleCompleteItem(id) {
-        completedForm = document.
-
-        toDoItems.find(item => {
-            if (item.id === id){
-                console.log(todo.content);
-            }
-        });
-    }
-
+ 
     //Add delete logic, eventListeners need to be added to Delete Buttons
     //Add completed logic, eventListenser need to be added to checkboxes
 
