cfec2ab4b88700e971c356294aa9c503898e5dcd
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-19 09:18:03 -0700

    Reading week 9

---
+++ week9/practice.js
@@ -0,0 +1,3 @@
+document.getElementById('height').textContent = 'Height: ' + window.screen.height;
+document.getElementById('width').textContent = 'Width: ' + window.screen.width;
+document.getElementById('color').textContent = 'Color Depth: ' + window.screen.colorDepth;
