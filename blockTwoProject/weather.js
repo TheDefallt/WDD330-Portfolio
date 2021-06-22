5113f806837281708b41ee0526d59cf8120ac823
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-22 16:37:41 -0700

    Update

--- blockTwoProject/weather.js
+++ blockTwoProject/weather.js
@@ -1,5 +1,6 @@
 const migraineButton = document.getElementById('migraineBtn');
 const currentTemp_elmnt = document.getElementById('currentTemp');
+const currentPressure_elmnt = document.getElementById('currentPressure');
 
 migraineButton.addEventListener('click', startWeatherFetch);
 
@@ -11,11 +12,14 @@ function fetchWeatherByCoord (pos) {
     let lat = pos.coords.latitude;
     let long = pos.coords.longitude;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=51c68784f1251d893077cc4f52143c83`)
+    let startTime = ~~(Date.now() / 1000) - 1440 * 60;
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${long}&dt=${startTime}&appid=51c68784f1251d893077cc4f52143c83`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        currentTemp_elmnt.textContent = kelvinToFaherenheit(data.main.temp);
+        currentTemp_elmnt.textContent = kelvinToFaherenheit(data.current.temp);
+        currentPressure_elmnt.textContent = pascalToMercury(data.current.pressure);
     });
 }
 
@@ -30,6 +34,14 @@ function fetchWeatherByCityState() {
     });
 }
 
+function fetchOneHourWeather(){
+
+}
+
 function kelvinToFaherenheit(temp){
     return Math.trunc((temp - 273.15) * 1.8 + 32);
+}
+
+function pascalToMercury(pressure){
+    return (pressure * 0.02953).toFixed(2);
 }
