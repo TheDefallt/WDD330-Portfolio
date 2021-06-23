72582be1948f51ec5fb1d3ac069d4907fe7f461f
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-22 20:41:39 -0700

    Update weather.js

--- blockTwoProject/weather.js
+++ blockTwoProject/weather.js
@@ -18,44 +18,42 @@ function fetchWeatherByCoord (pos) {
 
     let adjustedNow = ~~(Date.now() / 1000);
     let pastDay = adjustedNow - 1440 * 60;
-    console.log(adjustedNow);
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=51c68784f1251d893077cc4f52143c83`)
+    console.log("Requested Now: " + adjustedNow);
+
+    let currentData = "";
+    let pastData = "";
+
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=51c68784f1251d893077cc4f52143c83`)
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     currentData = data;
+    //     currentTemp_elmnt.textContent = kelvinToFaherenheit(data.main.temp);
+    //     currentPressure_elmnt.textContent = pascalToMercury(data.main.pressure);
+    // });
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${long}&dt=${adjustedNow}&appid=51c68784f1251d893077cc4f52143c83`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
         currentTemp_elmnt.textContent = kelvinToFaherenheit(data.current.temp);
         currentPressure_elmnt.textContent = pascalToMercury(data.current.pressure);
 
         let pastHourData = "";
 
         data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 7200){
+            console.log(adjustedNow - element.dt);
+            if(adjustedNow - element.dt <= 7200 && adjustedNow - element.dt >= 3600){
                 pastHourData = element;
+                // console.log(element);
             }
         });
 
         console.log(pastHourData);
-        hourTemp_elmnt.textContent = kelvinToFaherenheit(pastHourData.temp); //- kelvinToFaherenheit(data.current.temp);
-    });
-
-    fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${long}&dt=${pastDay}&appid=51c68784f1251d893077cc4f52143c83`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        currentTemp_elmnt.textContent = kelvinToFaherenheit(data.current.temp);
-        currentPressure_elmnt.textContent = pascalToMercury(data.current.pressure);
-
-        let pastHourData = "";
-
-        data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 7200){
-                pastHourData = element;
-            }
-        });
-
-        console.log(pastHourData);
-        hourTemp_elmnt.textContent = kelvinToFaherenheit(pastHourData.temp); //- kelvinToFaherenheit(data.current.temp);
+        hourTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(pastHourData.temp)},  
+        Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(pastHourData.temp)}`;
     });
 }
 
