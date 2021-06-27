e01305131fdf165428ca7a85c7609818ad117445
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-26 20:35:00 -0700

    Week 10

--- week10/quakes/quakeView.js
+++ week10/quakes/quakeView.js
@@ -2,15 +2,24 @@
 export default class QuakesView {
     renderQuakeList(quakeList, listElement) {
       //build a list of the quakes...include the title and time of each quake then append the list to listElement. You should also add the id of the quake record as a data- property to the li. ie. <li data-id="">
-      listElement.innerHTML = quakeList.features
-      .map(quake => {
-        return `
-  ${quake.properties.title}, ${new Date(
-          quake.properties.time
-        )}
-  `;
-      })
-      .join('');
+      console.log(quakeList);
+      listElement.innerHTML = '';
+     
+      quakeList.features.forEach(element => {
+        let date = new Date(element.properties.time);
+        let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        listElement.innerHTML += `<li data-id="${element.id}">${element.properties.title} - 
+        ${time}, ${date.toDateString()}</li>`;
+      });
+  //     listElement.innerHTML = quakeList.features
+  //     .map(quake => {
+  //       return `
+  // ${quake.properties.title}, ${new Date(
+  //         quake.properties.time
+  //       )}
+  // `;
+  //     })
+  //     .join('');
     }
     renderQuake(quake, element) {
       const quakeProperties = Object.entries(quake.properties);
