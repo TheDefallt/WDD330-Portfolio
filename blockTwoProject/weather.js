7dec580308b543f73b8c8871cbdbd1cd9029b705
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-28 17:28:03 -0700

    updates

--- blockTwoProject/weather.js
+++ blockTwoProject/weather.js
@@ -1,19 +1,20 @@
 const migraineButton = document.getElementById('migraineBtn');
+const hourContainer = document.getElementById('hourContainer');
 
 const currentTemp_elmnt = document.getElementById('currentTemp');
 const currentPressure_elmnt = document.getElementById('currentPressure');
 
-const hourTemp_elmnt = document.getElementById('hourTemp');
-const hourPressure_elmnt = document.getElementById('hourPressure');
+// const hourTemp_elmnt = document.getElementById('hourTemp');
+// const hourPressure_elmnt = document.getElementById('hourPressure');
 
-const threeTemp_elmnt = document.getElementById('threeTemp');
-const threePressure_elmnt = document.getElementById('threePressure');
+// const threeTemp_elmnt = document.getElementById('threeTemp');
+// const threePressure_elmnt = document.getElementById('threePressure');
 
-const sixTemp_elmnt = document.getElementById('sixTemp');
-const sixPressure_elmnt = document.getElementById('sixPressure');
+// const sixTemp_elmnt = document.getElementById('sixTemp');
+// const sixPressure_elmnt = document.getElementById('sixPressure');
 
-const twelveTemp_elmnt = document.getElementById('twelveTemp');
-const twelvePressure_elmnt = document.getElementById('twelvePressure');
+// const twelveTemp_elmnt = document.getElementById('twelveTemp');
+// const twelvePressure_elmnt = document.getElementById('twelvePressure');
 
 migraineButton.addEventListener('click', startWeatherFetch);
 
@@ -35,68 +36,81 @@ function fetchWeatherByCoord (pos) {
         console.log(data);
 
         /*---Current Data---*/
-        currentTemp_elmnt.textContent = kelvinToFaherenheit(data.current.temp);
-        currentPressure_elmnt.textContent = pascalToMercury(data.current.pressure);
+        currentTemp_elmnt.innerHTML = `${kelvinToFaherenheit(data.current.temp)}&#176F`;
+        currentPressure_elmnt.innerHTML = `${pascalToMercury(data.current.pressure)}in.`;
 
 
-        /*---Past Hour Data---*/
-        let pastHourData = "";
-
+        /*---Hour Looper---*/
         data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 7200 && adjustedNow - element.dt >= 3600){
-                pastHourData = element;
-            }
+            hourContainer.innerHTML += `
+            <div class="data-card">
+                <h2>Current Conditions</h2>
+                <div>${kelvinToFaherenheit(element.temp)}&#176F</div>
+                <div>${pascalToMercury(element.pressure)}in.</div>
+                <div>${tempDifference(data.current.temp, element.temp)}&#176F</div>
+                <div>${pressureDifference(data.current.pressure, element.pressure)}in.</div>
+            </div>
+            `;
         });
+    })
+    //     /*---Past Hour Data---*/
+    //     let pastHourData = "";
 
-        hourTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(pastHourData.temp)},  
-        Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(pastHourData.temp)}`;
-        hourPressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(pastHourData.pressure)},
-        Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(pastHourData.pressure)).toFixed(2)}`;
+    //     data.hourly.forEach(element => {
+    //         if(adjustedNow - element.dt <= 7200 && adjustedNow - element.dt >= 3600){
+    //             pastHourData = element;
+    //         }
+    //     });
 
-
-        /*---Past Three Hours Data---*/
-        let threeHourData = "";
-
-        data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 10800 && adjustedNow - element.dt >= 7200){
-                threeHourData = element;
-            }
-        });
-
-        threeTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(threeHourData.temp)},  
-        Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(threeHourData.temp)}`;
-        threePressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(threeHourData.pressure)},
-        Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(threeHourData.pressure)).toFixed(2)}`;
-
-        /*---Past Six Hours Data---*/
-        let sixHourData = "";
-
-        data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 25200 && adjustedNow - element.dt >= 21600){
-                sixHourData = element;
-            }
-        });
-
-        sixTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(sixHourData.temp)},  
-        Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(sixHourData.temp)}`;
-        sixPressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(sixHourData.pressure)},
-        Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(sixHourData.pressure)).toFixed(2)}`;
+    //     hourTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(pastHourData.temp)},  
+    //     Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(pastHourData.temp)}`;
+    //     hourPressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(pastHourData.pressure)},
+    //     Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(pastHourData.pressure)).toFixed(2)}`;
 
 
-        /*---Past Twelve Hours Data---*/
-        let twelveHourData = "";
+    //     /*---Past Three Hours Data---*/
+    //     let threeHourData = "";
 
-        data.hourly.forEach(element => {
-            if(adjustedNow - element.dt <= 46800 && adjustedNow - element.dt >= 43200){
-                twelveHourData = element;
-            }
-        });
+    //     data.hourly.forEach(element => {
+    //         if(adjustedNow - element.dt <= 10800 && adjustedNow - element.dt >= 7200){
+    //             threeHourData = element;
+    //         }
+    //     });
 
-        twelveTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(twelveHourData.temp)},  
-        Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(twelveHourData.temp)}`;
-        twelvePressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(twelveHourData.pressure)},
-        Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(twelveHourData.pressure)).toFixed(2)}`;
-    });
+    //     threeTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(threeHourData.temp)},  
+    //     Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(threeHourData.temp)}`;
+    //     threePressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(threeHourData.pressure)},
+    //     Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(threeHourData.pressure)).toFixed(2)}`;
+
+    //     /*---Past Six Hours Data---*/
+    //     let sixHourData = "";
+
+    //     data.hourly.forEach(element => {
+    //         if(adjustedNow - element.dt <= 25200 && adjustedNow - element.dt >= 21600){
+    //             sixHourData = element;
+    //         }
+    //     });
+
+    //     sixTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(sixHourData.temp)},  
+    //     Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(sixHourData.temp)}`;
+    //     sixPressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(sixHourData.pressure)},
+    //     Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(sixHourData.pressure)).toFixed(2)}`;
+
+
+    //     /*---Past Twelve Hours Data---*/
+    //     let twelveHourData = "";
+
+    //     data.hourly.forEach(element => {
+    //         if(adjustedNow - element.dt <= 46800 && adjustedNow - element.dt >= 43200){
+    //             twelveHourData = element;
+    //         }
+    //     });
+
+    //     twelveTemp_elmnt.textContent = `Previous Temp:${kelvinToFaherenheit(twelveHourData.temp)},  
+    //     Difference:${kelvinToFaherenheit(data.current.temp) - kelvinToFaherenheit(twelveHourData.temp)}`;
+    //     twelvePressure_elmnt.textContent = `Previous Pressure:${pascalToMercury(twelveHourData.pressure)},
+    //     Difference:${(pascalToMercury(data.current.pressure) - pascalToMercury(twelveHourData.pressure)).toFixed(2)}`;
+    // });
 }
 
 function fetchWeatherByCityState() {
@@ -115,4 +129,12 @@ function kelvinToFaherenheit(temp){
 
 function pascalToMercury(pressure){
     return (pressure * 0.02953).toFixed(2);
+}
+
+function tempDifference(currentTemp, pastTemp){
+    return kelvinToFaherenheit(currentTemp) - kelvinToFaherenheit(pastTemp);
+}
+
+function pressureDifference(currentPressure, pastPressure){
+    return (pascalToMercury(currentPressure) - pascalToMercury(pastPressure)).toFixed(2);
 }
