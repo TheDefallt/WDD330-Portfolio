d4a93580edf78be1cf6f32a1bc1c9d7cc1b6d2d1
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-30 17:25:34 -0700

    Improvements

--- blockTwoProject/weather.js
+++ blockTwoProject/weather.js
@@ -5,6 +5,7 @@ migraineButton.addEventListener('click', startWeatherFetch);
 
 function startWeatherFetch(){
     window.navigator.geolocation.getCurrentPosition(fetchWeatherByCoord);
+    document.getElementById('btnContainer').classList.add("hide");
 }
 
 function fetchWeatherByCoord (pos) {
@@ -28,6 +29,20 @@ function fetchWeatherByCoord (pos) {
         </div>
         `;
 
+        /*---Add radio buttons---*/
+        hourContainer.innerHTML += `
+        <h2>Compare To:</h2>
+        <input type="radio" value="Now">
+        <label for="now">Now</label>
+        <input type="radio" value="HourByHour">
+        <label for="hourbyhour">Hour-by-Hour</label>
+        `;
+        
+        /*---Add Reset Button---*/
+        hourContainer.innerHTML += `
+        <button class="resetBtn" id="resetBtn">Reset</button>
+        `;
+
         /*---Hour Looper---*/
         for(let i=data.hourly.length - 1; i > 0; i--){
             hourContainer.innerHTML += `
@@ -40,8 +55,14 @@ function fetchWeatherByCoord (pos) {
             </div>
             `;
         }
+        // callback(hourLoopNow);
+    })
+    .then( () => {
+        document.getElementById('resetBtn').addEventListener('click', reset);
     })
 }
+
+
 
 function kelvinToFaherenheit(temp){
     return Math.trunc((temp - 273.15) * 1.8 + 32);
@@ -57,4 +78,37 @@ function tempDifference(currentTemp, pastTemp){
 
 function pressureDifference(currentPressure, pastPressure){
     return (pascalToMercury(currentPressure) - pascalToMercury(pastPressure)).toFixed(2);
+}
+
+function hourLoopNow(data) {
+    for(let i=data.hourly.length - 1; i > 0; i--){
+        hourContainer.innerHTML += `
+        <div class="data-card">
+            <h2>Conditions at: ${new Date(data.hourly[i].dt * 1000).toLocaleTimeString()}</h2>
+            <div>Meas: ${kelvinToFaherenheit(data.hourly[i].temp)}&#176F</div>
+            <div>${pascalToMercury(data.hourly[i].pressure)}in.</div>
+            <div>Diff: ${tempDifference(data.current.temp, data.hourly[i].temp)}&#176F</div>
+            <div>${pressureDifference(data.current.pressure, data.hourly[i].pressure)}in.</div>
+        </div>
+        `;
+    }
+}
+
+function hourLoopByHour(data) {
+    for(let i=data.hourly.length - 1; i > 0; i--){
+        hourContainer.innerHTML += `
+        <div class="data-card">
+            <h2>Conditions at: ${new Date(data.hourly[i].dt * 1000).toLocaleTimeString()}</h2>
+            <div>Meas: ${kelvinToFaherenheit(data.hourly[i].temp)}&#176F</div>
+            <div>${pascalToMercury(data.hourly[i].pressure)}in.</div>
+            <div>Diff: ${tempDifference(data.current.temp, data.hourly[i].temp)}&#176F</div>
+            <div>${pressureDifference(data.current.pressure, data.hourly[i].pressure)}in.</div>
+        </div>
+        `;
+    }
+}
+
+function reset(){
+    hourContainer.innerHTML = ``;
+    document.getElementById("btnContainer").classList.remove("hide");
 }
